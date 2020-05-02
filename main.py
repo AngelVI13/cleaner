ff4420d461ab76d161f6bd050e0da4f8150111fb
Angel ILIEV <a.v.iliev13@gmail.com>
2020-05-02 16:50:16 +0300

    Added recursive graph update on click

--- main.py
+++ main.py
@@ -38,6 +38,9 @@ def get_dir_info(directory: str) -> Dict[str, int]:
     :param directory: Target directory which to get info for
     :return: Dictionary with file/dir and corresponding byte size
     """
+    if not os.path.isdir(directory):
+        return {}
+
     items = os.listdir(directory)
     items = [os.path.join(directory, item) for item in items]
 
@@ -54,9 +57,53 @@ def get_dir_info(directory: str) -> Dict[str, int]:
 
 class DirPlot:
     def __init__(self, directory):
-        self.directory = directory
-        self.size = get_dir_size(directory)
+        self.__directory = directory
+        self.__size = get_dir_size(directory)
+        self._create_plot()
+
         self.show_figure()
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, _value):
+        raise RuntimeError("Setting `size` attribute directly not allowed.")
+
+    @property
+    def directory(self):
+        return self.__directory
+
+    @directory.setter
+    def directory(self, path):
+        self.__directory = path
+        self.__size = get_dir_size(path)
+
+    def _create_plot(self):
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot(111)
+
+    def _update_plot_data(self):
+        # Clear existing data on plot
+        self.ax.clear()
+
+        # Calculate directory content info
+        content_info = get_dir_info(self.directory)
+        content_info = self._format_dir_info(content_info)
+
+        # Set plot title
+        title = f"Path: {self.directory} ({get_size_str(self.size)})"
+        self.ax.set_title(title)
+
+        # Calculate chart item proportions
+        labels = content_info.keys()
+        dir_size = sum(content_info.values())
+        sizes = [(size / dir_size) * 100 for size in content_info.values()]
+        wedges, plt_labels = self.ax.pie(sizes, labels=labels)
+        self.ax.axis("equal")
+
+        self.make_picker(self.fig, wedges)
 
     def _format_dir_info(self, dir_info: Dict[str, int]) -> Dict[str, int]:
         # exclude empty directories
@@ -70,30 +117,22 @@ class DirPlot:
         return info
 
     def show_figure(self) -> None:
-        content_info = get_dir_info("..")
-        content_info = self._format_dir_info(content_info)
+        self._update_plot_data()
 
-        # Make an example pie plot
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-
-        title = f"Path: {self.directory} ({self.size})"
-        ax.set_title(title)
-
-        labels = content_info.keys()
-        dir_size = sum(content_info.values())
-        sizes = [(size / dir_size) * 100 for size in content_info.values()]
-        wedges, plt_labels = ax.pie(sizes, labels=labels)
-        ax.axis("equal")
-
-        self.make_picker(fig, wedges)
-        plt.show()
+        try:
+            plt.show()
+        except AttributeError:
+            # For some reason when closing diagram it throws AttributeError
+            pass
 
     def make_picker(self, fig, wedges):
         def onclick(event):
             wedge = event.artist
             label = wedge.get_label()
-            print(label)
+
+            path, *_ = label.split("\n")
+            self.directory = path
+            self.show_figure()
 
         # Make wedges selectable
         for wedge in wedges:
